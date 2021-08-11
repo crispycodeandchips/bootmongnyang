f478e77b6e8b279de4a1ae79bd5203415ddb4a96
crispycodeandchips <bb9e.bb8.c3po.r2d2@gmail.com>
2021-08-11 21:06:14 +0900

    initial spring setup

---
+++ src/main/java/com/example/bootmongnyang/BootmongnyangApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootmongnyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootmongnyangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootmongnyangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bootmongnyang/BootmongnyangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootmongnyang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootmongnyangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
